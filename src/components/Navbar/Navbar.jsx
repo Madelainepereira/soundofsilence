ec8ad44e0c62b3899575ee543da236325e621211
MiriV <mirivf@hotmail.es>
2023-10-02 14:27:36 +0200

    navbar created

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-content">
+        <div className="navbar-title">
+          <span className="bold">Sound of</span>
+          <br />
+          <span>Silence</span>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
