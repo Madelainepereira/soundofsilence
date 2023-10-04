b738cf42bf3c34fa005c76632462e029ace5a142
MiriV <mirivf@hotmail.es>
2023-10-04 15:17:29 +0200

    created button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
+import Button from '../src/components/Button/Button'
 
 function App() {
   
 
   return (
     <>
+    <Button/>
+    </>
+
+    
 
         
-    </>
+    
   )
 }
 
