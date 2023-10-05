27ad80788b53a8e5decc2d1bcb4cc633d375c142
madelainepereira <madelaineasuaje@gmail.com>
2023-10-05 12:33:51 +0200

    clean app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import Body from './components/Body/Body'
+
 
 function App() {
   
 
   return (
     <>
-      <Navbar/>
-      <Body/>  
+
     </>
 
     
