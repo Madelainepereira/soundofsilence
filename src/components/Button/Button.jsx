201f95708bb4dac85fba7c42a250ac51bcd422a1
MiriV <mirivf@hotmail.es>
2023-10-05 14:28:05 +0200

    recorded in button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,26 +1,54 @@
 import { useState } from 'react';
-import Recorder from "../Recorder";
-
 import './button.css'
 import oreja from "../../assets/Mask_group.png";
 
+const Button = () => {
+    const [recording, setRecording] = useState(false);
+    const [mediaRecorder, setMediaRecorder] = useState(null);
+    const [audioChunks, setAudioChunks] = useState([]);
 
+    const startRecording = async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const recorder = new MediaRecorder(stream);
 
-function Button(){
-    const [showRecorder, setShowRecorder] = useState(false);
+        recorder.ondataavailable = (e) => {
+            if (e.data.size > 0) {
+                setAudioChunks((prev) => [...prev, e.data]);
+            }
+        };
 
-    const toggleRecorder = () => {
-        setShowRecorder(!showRecorder);
+        recorder.onstop = () => {
+            const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+            const audioUrl = URL.createObjectURL(audioBlob);
+            const audio = new Audio(audioUrl);
+            audio.play();
+        };
+
+        recorder.start();
+        setRecording(true);
+        setMediaRecorder(recorder);
+
+        setTimeout(() => {
+            stopRecording();
+        }, 15000); 
     };
+
+    const stopRecording = () => {
+        if (mediaRecorder) {
+            mediaRecorder.stop();
+            setRecording(false);
+        }
+    };
+
+
 
     return(
         <> 
-            <button className="button" onClick={toggleRecorder}>
+            <button className="button" onClick={startRecording} disabled={recording}>
                 PULSAR PARA GRABAR
                 <img src={oreja} alt='Icono de oreja'/>
             </button>
 
-            {showRecorder && <Recorder />}
         </>
        
     );
@@ -28,4 +56,4 @@ function Button(){
 
 
 
-export default Button;
+export default Button
