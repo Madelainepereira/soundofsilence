15f823e5bf79dcd8e63b6d38365b763a7c2ab66b
MiriV <mirivf@hotmail.es>
2023-10-05 14:20:06 +0200

    recorder out of button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import './App.css'
+import Navbar from "../src/components/Navbar/Navbar"
+import Body from "../src/components/Body/Body"
+import Button from "../src/components/Button/Button"
 
 
 function App() {
@@ -6,6 +9,9 @@ function App() {
 
   return (
     <>
+      <Navbar/>
+      <Body/>
+      <Button/>
 
     </>
 
