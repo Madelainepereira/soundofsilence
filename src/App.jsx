c05a36fd88b60c433318f82df1497858e9703520
MiriV <mirivf@hotmail.es>
2023-10-11 13:04:57 +0200

    app deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
-import Navbar from "../src/components/Navbar/Navbar"
-import Body from "../src/components/Body/Body"
-import Footer from './components/Footer/Footer'
+
 
 function App() {
   
 
   return (
     <>
-      <Navbar/>
-      <Body/>
-      <Footer/>
+      
     </>
 
     
