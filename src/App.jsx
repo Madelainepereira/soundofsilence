5903f9e5604e79f9daaf1e66e32beacecbc66bf8
MiriV <mirivf@hotmail.es>
2023-10-11 13:03:58 +0200

    footer created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from "../src/components/Navbar/Navbar"
 import Body from "../src/components/Body/Body"
+import Footer from './components/Footer/Footer'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar/>
       <Body/>
+      <Footer/>
     </>
 
     
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+      <div className="footer">
+        <p>Privacy policy</p>
+        <p>Terms and Conditions</p>
+      </div>
+    );
+  }
+  
+  export default Footer;
