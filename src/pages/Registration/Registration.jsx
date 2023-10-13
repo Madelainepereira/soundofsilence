b45f92795230d1b1c17e553833d701e2d478fbae
madelainepereira <madelaineasuaje@gmail.com>
2023-10-13 09:37:20 +0200

    Registration done

---
+++ src/pages/Registration/Registration.jsx
@@ -0,0 +1,97 @@
+import './Registration.css'
+import { useState } from 'react';
+
+function Registration () {
+
+	const [formData, setFormData] = useState({
+		nombre: '',
+		apellido: '',
+		username: '',
+		email: '',
+		password: '',
+		confirmPassword: '',
+	});
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// Aquí se puede agregar la lógica para manejar los datos del formulario
+	};
+
+	return(
+		<>
+			<div className='welcomeText'>
+				<h1>Bienvenid@</h1>
+				<h4>Ingresa los siguientes datos y crea tu cuenta</h4>
+			</div>
+		<form className='registrationForm' onSubmit={handleSubmit}>
+    <div className='formGroup'>
+          <label className='identifier'>Nombre</label>
+          <input className='registrationBox'
+            type='text'
+            name='nombre'
+            value={formData.nombre}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className='formGroup'>
+          <label className='identifier'>Apellido</label>
+          <input className='registrationBox'
+            type='text'
+            name='apellido'
+            value={formData.apellido}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className='formGroup'>
+          <label className='identifier'>Nombre de usuario</label>
+          <input className='registrationBox'
+            type='text'
+            name='username'
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className='formGroup'>
+          <label className='identifier'>Correo Electrónico</label>
+          <input className='registrationBox'
+            type='email'
+            name='email'
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className='formGroup'>
+          <label className='identifier'>Contraseña</label>
+          <input className='registrationBox'
+            type='password'
+            name='password'
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className='formGroup'>
+          <label className='identifier'>Confirmar Contraseña</label>
+          <input className='registrationBox'
+            type='password'
+            name='confirmPassword'
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button className='registrationButton' type='submit'>Registrar</button>
+      </form>
+		</>
+	);
+}
+export default Registration;
