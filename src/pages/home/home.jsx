bdc29aaeb6a53a5bc47f6c85fb8d705f95bedf42
madelainepereira <madelaineasuaje@gmail.com>
2023-10-14 10:20:41 +0200

    delete routes from Home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,16 +6,11 @@ import HomeLayout from '../../layouts/Homelayout/Homelayout';
 const Home = () => {
 
   return (
-      <HomeLayout>
-        {<div className='home'> 
-          <div className='homeimage'><img className='logohome' src={logo} alt="Image sound of silence" /></div>
-      
+          <>
+          <div className='homeimage'><img className='logohome' src={logo} alt="Image sound of silence" /></div>  
           <Button2 text="Login" />
-          <Button2 text="Sign Up" />
-      </div>}
-      </HomeLayout>
-      
-    
+          <Button2 text="Sign Up" />      
+          </>
   );
 }
 
