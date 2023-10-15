31d3abcce4848e9dc78fb82487a6fad9ca4c7305
madelainepereira <madelaineasuaje@gmail.com>
2023-10-15 10:11:55 +0200

    Change of the navigation bar logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import './Navbar.css';
-import logo from '../../assets/logo.png';
+import Logohome from '../../assets/Logohome.png';
+
 
 function Navbar() {
   return (
     <nav className="navbar">
-        <img src={logo} alt="Logo" />
+        <img className='navbarLogo' src={Logohome} alt="Logo" />
     </nav>
   );
 }
