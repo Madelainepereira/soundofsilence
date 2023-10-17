2a60229f46c2d06cbab2d953886725b15316038c
madelainepereira <madelaineasuaje@gmail.com>
2023-10-17 10:56:55 +0200

    View under construction

---
+++ src/components/HistoryCards/HistoryCards.jsx
@@ -0,0 +1,12 @@
+import './HistoryCards.css'
+
+function HistoryCards() {
+	return(
+		<>
+			<div className='Cards'>
+				<div className='labelCard'></div>
+			</div>
+		</>
+	)
+}
+export default HistoryCards;
