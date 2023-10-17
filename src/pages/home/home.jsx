b12e474f7e11c4c890e159e6f3828545b558e831
madelainepereira <madelaineasuaje@gmail.com>
2023-10-17 10:48:09 +0200

    new logo and new modifications in Button2

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,6 @@
 import './home.css'
-import Logohome from'../../assets/Logohome.png'
 import Button2 from '../../components/Button2/Button2';
+import logo from '../../assets/logo.svg'
 //import HomeLayout from '../../layouts/Homelayout/Homelayout';
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
           <>
           <div className='overheadSpace'></div>
           <main className='contentHome'>
-            <div className='homeimage'><img className='logohome' src={Logohome} alt="Image sound of silence" /></div>  
+            <div className='homeimage'><img className='logohome' src={logo} alt="Image sound of silence" /></div>  
             <Button2 text="Login" />
             <Button2 text="Sign Up" />    
           </main>  
