77a41d5d04d46336e6fcdb8ac757356846bb7805
Anais-RV <anaisrv@gmail.com>
2023-10-20 19:43:35 +0200

    token fixed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,27 +1,3 @@
-// import PropTypes from 'prop-types';
-// import './Card.css';
-
-// function Card({texto, className}) {
-//   //const { texto, className } = props;
-
-//   return (
-//     <div className="textCard">
-//       <p  className={className}>
-//         {texto}
-//       </p>
-//     </div>
-//   );
-// }
-
-// Card.propTypes = 
-// {
-//   texto: PropTypes.string.isRequired,
-//   className: PropTypes.string, // Define la prop className como opcional
-// };
-
-// export default Card;
-
-// Card.jsx
 import PropTypes from 'prop-types';
 import './Card.css';
 
