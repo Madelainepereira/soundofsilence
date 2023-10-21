918f4188c107644b778707b948ee09a2b1fe8ddb
madelainepereira <madelaineasuaje@gmail.com>
2023-10-21 14:27:44 +0200

    remove unused route

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Layout from '../layouts/Homelayout/Layout';
 import Login from '../pages/Login/Login'
-import AudioProcess from '../pages/AudioProcess/AudioProcess'
+// import AudioProcess from '../pages/AudioProcess/AudioProcess'
 import History from '../pages/History/History'
 import Home from '../pages/Home/Home'
 import LabelResults from '../pages/LabelResults/LabelResults'
@@ -16,10 +16,10 @@ const RouterItem = () => {
             <Routes>
                 <Route path='/' element={<Layout/>}>
                 <Route path='/Login' element={<Login/>}></Route>
-                <Route path='/AudioProcess' element={<AudioProcess/>}></Route>
+                {/* <Route path='/AudioProcess' element={<AudioProcess/>}></Route> */}
                 <Route path='/History' element={<History/>}></Route>
                 <Route path='/:user?' element={<Home/>}></Route>
-                {/* <Route path='/LabelResults' element={<LabelResults/>}></Route> */}
+                <Route path='/LabelResults' element={<LabelResults/>}></Route>
                 <Route path='/LabelResults/:audioId' element={<LabelResults />} />
                 <Route path='/Registration' element={<Registration/>}></Route>
                 <Route path='/UserView' element={<UserView/>}></Route>
